4b8455050df32091e1f727e25e6215731ac24c21
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-09-10 04:10:24 +0800

    feat(bindgen): associated enum

--- crates/libs/bindgen/src/tables/field.rs
+++ crates/libs/bindgen/src/tables/field.rs
@@ -6,6 +6,23 @@ pub trait FieldExt {
 
 impl FieldExt for Field {
     fn field_type(&self, enclosing: Option<&CppStruct>, reader: &Reader) -> Type {
+        if let Some(enclosing) = enclosing {
+            for attribute in self.attributes() {
+                if attribute.name() == "AssociatedEnumAttribute"
+                    && let Some((_, Value::Utf8(enum_name))) = attribute.value().first()
+                {
+                    return Type::from_metadata_type(
+                        &windows_metadata::Type::ValueName(windows_metadata::TypeName::named(
+                            enclosing.def.namespace(),
+                            enum_name,
+                        )),
+                        Some(enclosing),
+                        &[],
+                        reader,
+                    );
+                }
+            }
+        }
         Type::from_metadata_type(&self.ty(), enclosing, &[], reader)
     }
 }
